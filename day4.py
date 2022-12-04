f7d182a4fdc9f61bda8fcd8a09b0f065e1039a08
BAAAKA <baaakaDev@protonmail.ch>
2022-12-04 09:38:38 +0100

    day4 wip

--- day4.py
+++ day4.py
@@ -1,4 +1,7 @@
 
+
+
+z = 0
 with open('tempInput.txt') as f:
     f = f.read().strip().split("\n")
 
@@ -7,3 +10,9 @@ with open('tempInput.txt') as f:
         x1 = temp[0].split("-")
         x2 = temp[1].split("-")
         print(f"{x1} and {x2}")
+        if x1[0] < x2[0] and x1[1] > x2[1]:
+            z+=1
+        elif x2[0] < x1[0] and x2[1] > x1[1]:
+            z+=1
+
+print(z)
