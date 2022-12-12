dc900b96625c5b1a27e6b7e9ccbb41612b091656
BAAAKA <baaakaDev@protonmail.ch>
2022-12-12 23:17:22 +0100

    day 12... some more

--- day12.py
+++ day12.py
@@ -1,12 +1,21 @@
 
 
-height = ['abcdefghijklmnopqrstuvwxyz']
+height = 'Sabcdefghijklmnopqrstuvwxyz_0'
 
 def grid(row, col, maxRow, maxCol):
     if row < 0 or row > maxRow-1 or col < 0 or col > maxCol-1:
-        return 30
+        return '0'
     else:
         return f[row][col]
+
+def letToNr(letter):
+    return height.index(letter)
+
+position = [0, 0]
+path = {'0,0'}
+
+def canIgoThere(currentL, targetL):
+    return letToNr(currentL) - letToNr(targetL) >= -1
 
 with open('tempInput.txt') as f:
     f = f.read().split("\n")
@@ -17,5 +26,39 @@ with open('tempInput.txt') as f:
         #print(f[row])
         for col in range(maxCol):
             #print(f"{row}, {col}")
-            print(f[row][col], end="")
+            print(grid(row, col, maxRow, maxCol), end="")
         print("")
+
+for x in range(10):
+    row = position[0]
+    col = position[1]
+    current = grid(row, col, maxRow, maxCol)
+    up = grid(row-1, col, maxRow, maxCol)
+    down = grid(row+1, col, maxRow, maxCol)
+    left = grid(row, col-1, maxRow, maxCol)
+    right = grid(row, col+1, maxRow, maxCol)
+    print(f"{up} {down} {left} {right}")
+    print(f"{canIgoThere(current, up)} {canIgoThere(current, down)} {canIgoThere(current, left)} {canIgoThere(current, right)}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
