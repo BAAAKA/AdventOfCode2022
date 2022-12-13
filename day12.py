82c8f67964779782d1dd83e21df61951fb6b844a
BAAAKA <baaakaDev@protonmail.ch>
2022-12-13 11:10:10 +0100

    wuuuuu day 12 done

--- day12.py
+++ day12.py
@@ -9,27 +9,21 @@ def grid(row, col, maxRow, maxCol):
         return f[row][col]
 
 def letToNr(letter):
+    if letter == "E":
+        letter = 'z'
     return height.index(letter)
 
-position = [0, 0]
-path = {'0,0'}
+def posToStr(position):
+    return '{},{}'.format(*position)
 
+lastPosition = [[20, 158]]
+previousPositions = {'0,0'}
+
+# 31-4
 def canIgoThere(currentL, targetL):
-    return letToNr(currentL) - letToNr(targetL) >= -1
+    return letToNr(currentL) - letToNr(targetL) <= 1
 
-with open('tempInput.txt') as f:
-    f = f.read().split("\n")
-    maxRow = len(f)
-    maxCol = len(f[0])
-
-    for row in range(maxRow):
-        #print(f[row])
-        for col in range(maxCol):
-            #print(f"{row}, {col}")
-            print(grid(row, col, maxRow, maxCol), end="")
-        print("")
-
-for x in range(10):
+def christmas(position, previousPositions):
     row = position[0]
     col = position[1]
     current = grid(row, col, maxRow, maxCol)
@@ -37,17 +31,63 @@ for x in range(10):
     down = grid(row+1, col, maxRow, maxCol)
     left = grid(row, col-1, maxRow, maxCol)
     right = grid(row, col+1, maxRow, maxCol)
-    print(f"{up} {down} {left} {right}")
-    print(f"{canIgoThere(current, up)} {canIgoThere(current, down)} {canIgoThere(current, left)} {canIgoThere(current, right)}")
+
+    if current == "a":
+        print(f"FOUND a")
+        exit()
+
+    #print(f"{up} {down} {left} {right}")
+    #print(f"{canIgoThere(current, up)} {canIgoThere(current, down)} {canIgoThere(current, left)} {canIgoThere(current, right)}")
+    possibilities = []
+    if canIgoThere(current, up):
+        possibilities.append([row-1, col])
+    if canIgoThere(current, down):
+        possibilities.append([row+1, col])
+    if canIgoThere(current, left):
+        possibilities.append([row, col-1])
+    if canIgoThere(current, right):
+        possibilities.append([row, col+1])
+    #print(f"all possibleilities {possibilities}")
+    returnAra = []
+    for pos in possibilities:
+        posStr = posToStr(pos)
+        if posStr not in previousPositions:
+            returnAra.append(pos)
+    return returnAra
+
+def callChristmas(positions, previousPositions):
+    nextPositions = []
+    for position in positions:
+        nextPositions += christmas(position, previousPositions)
+        for nPos in nextPositions:
+            previousPositions.add(posToStr(nPos))
+    #print(f"nextPositions {nextPositions}")
+    return nextPositions
+
+def printMap(positions):
+    for row in range(maxRow):
+        #print(f[row])
+        for col in range(maxCol):
+            #print(f"{row}, {col}")
+            if [row, col] in positions:
+                print("0", end="")
+            else:
+                print(grid(row, col, maxRow, maxCol), end="")
+        print("")
+
+with open('tempInput.txt') as f:
+    f = f.read().split("\n")
+    maxRow = len(f)
+    maxCol = len(f[0])
 
 
-
-
-
-
-
-
-
+print("###################")
+for i in range(1000):
+    print(f"Santa says its day {i}")
+    lastPosition = callChristmas(lastPosition, previousPositions)
+    print(f"{lastPosition} {previousPositions}")
+    if i%30==0:
+        printMap(lastPosition)
 
 
 
--- main.py
+++ main.py
@@ -1,3 +1,9 @@
-Test
-
-
+with open('tempInput.txt') as f:
+    f = f.read().split("\n")
+    maxRow = len(f)
+    maxCol = len(f[0])
+    for row in range(maxRow):
+        for col in range(maxCol):
+            #print(f"{f[row][col]}")
+            if f[row][col] == 'E':
+                print(f"found E at [{row}, {col}]")
